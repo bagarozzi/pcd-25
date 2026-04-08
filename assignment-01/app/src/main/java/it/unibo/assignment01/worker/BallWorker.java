c4cd840c0c4801f280e3aedf7278e23179d96db1
Federico Bagattoni <federicobagattoni61@gmail.com>
2026-04-08 16:56:59 +0200

    Added BallWorker class for workers calculating collisions an movements

---
+++ assignment-01/app/src/main/java/it/unibo/assignment01/worker/BallWorker.java
@@ -0,0 +1,13 @@
+package it.unibo.assignment01.worker;
+
+public class BallWorker extends Thread {
+    
+    // TODO: add fields for balls and barrier
+
+    public BallWorker(/* TODO: add parameters */) { }
+
+    @Override
+    public void run() {
+        // TODO: Implement loop for first moving and computing collisions
+    }
+}
