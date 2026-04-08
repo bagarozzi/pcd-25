24a27ab7afe06e39e45c7014c74dfcff96329174
Luca Venturini <luca.venturini14@studio.unibo.it>
2026-04-08 16:54:02 +0200

    copied bounded buffer

---
+++ assignment-01/app/src/main/java/it/unibo/assignment01/util/BoundedBuffer.java
@@ -0,0 +1,9 @@
+package it.unibo.assignment01.util;
+
+public interface BoundedBuffer<Item> {
+
+    void put(Item item) throws InterruptedException;
+    
+    Item get() throws InterruptedException;
+    
+}
