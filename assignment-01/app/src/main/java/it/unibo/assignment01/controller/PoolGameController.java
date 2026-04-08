280fab5b0ea06bc8db3ccfba0a4a0e91e37e1e16
Federico Bagattoni <federicobagattoni61@gmail.com>
2026-04-08 18:08:04 +0200

    Made the Controller a thread and added a main loop for updates

--- assignment-01/app/src/main/java/it/unibo/assignment01/controller/PoolGameController.java
+++ assignment-01/app/src/main/java/it/unibo/assignment01/controller/PoolGameController.java
@@ -1,11 +1,42 @@
 package it.unibo.assignment01.controller;
 
-public class PoolGameController implements Controller{
+public class PoolGameController extends Thread implements Controller {
+
+    public PoolGameController() {
+
+    }
 
     @Override
     public void start() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+
+    @Override
+    public void run() {
+		int nFrames = 0;
+		long t0 = System.currentTimeMillis();
+		long lastUpdateTime = System.currentTimeMillis();
+		
+		while (true){			
+		
+			/// Upgrade ball movements and collisions, knowing the last time the board was updated and the current time.
+			
+			long elapsed = System.currentTimeMillis() - lastUpdateTime;
+			lastUpdateTime = System.currentTimeMillis();			
+
+			
+			nFrames++;
+			int framePerSec = 0;
+			long dt = (System.currentTimeMillis() - t0);
+			if (dt > 0) {
+				framePerSec = (int)(nFrames*1000/dt);
+			}
+
+            // Render the view after calculating how many frames have passed during the calculation
+			
+		}
+
     }
 
     
