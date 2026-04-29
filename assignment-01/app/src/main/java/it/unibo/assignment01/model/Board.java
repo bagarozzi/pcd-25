2aef2569dfb242e5611f0ba128328fee8ebf3e0c
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-04-29 11:32:54 +0200

    Added board interface

--- assignment-01/app/src/main/java/it/unibo/assignment01/model/Board.java
+++ assignment-01/app/src/main/java/it/unibo/assignment01/model/Board.java
@@ -1,5 +1,13 @@
 package it.unibo.assignment01.model;
 
+import java.util.List;
+
 public interface Board {
+
+    public List<Ball> getBalls();
+    
+    public Ball getPlayerBall();
+    
+    public  Boundary getBounds();
     
 }
