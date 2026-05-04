996233259d6e2ba007f292065709cd06eb128cc6
Federico Bagattoni <federicobagattoni61@gmail.com>
2026-05-04 14:51:12 +0200

    Partial implementation of main class

--- assignment-01/app/src/main/java/it/unibo/assignment01/Poool.java
+++ assignment-01/app/src/main/java/it/unibo/assignment01/Poool.java
@@ -1,7 +1,18 @@
 package it.unibo.assignment01;
 
+import it.unibo.assignment01.controller.PoolGameController;
+import it.unibo.assignment01.view.View;
+import it.unibo.assignment01.controller.Controller;
+
 public class Poool {
+
+    private static final int FRAME_WIDTH = 800;
+    private static final int FRAME_HEIGHT = 600;
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Controller controller = new PoolGameController();
+        View view = new View(FRAME_WIDTH, FRAME_HEIGHT);
+        view.setController(controller);
+        controller.start();
+        view.display();
     }
 }
