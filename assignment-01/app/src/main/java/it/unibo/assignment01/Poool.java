968822a867af64271cfdb476d451ac5738b2bd18
Federico Bagattoni <federicobagattoni61@gmail.com>
2026-05-04 14:57:36 +0200

    Modified main class implementation

--- assignment-01/app/src/main/java/it/unibo/assignment01/Poool.java
+++ assignment-01/app/src/main/java/it/unibo/assignment01/Poool.java
@@ -9,8 +9,8 @@ public class Poool {
     private static final int FRAME_WIDTH = 800;
     private static final int FRAME_HEIGHT = 600;
     public static void main(String[] args) {
-        Controller controller = new PoolGameController();
         View view = new View(FRAME_WIDTH, FRAME_HEIGHT);
+        Controller controller = new PoolGameController(view);
         view.setController(controller);
         controller.start();
         view.display();
