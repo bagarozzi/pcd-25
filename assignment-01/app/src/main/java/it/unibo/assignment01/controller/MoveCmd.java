4e1be8824d75fdcaf073a3c5fff10b0a0a8e0eff
Luca Venturini <luca.venturini14@studio.unibo.it>
2026-05-04 14:44:50 +0200

    implemented movement command per player ball

--- assignment-01/app/src/main/java/it/unibo/assignment01/controller/MoveCmd.java
+++ assignment-01/app/src/main/java/it/unibo/assignment01/controller/MoveCmd.java
@@ -1,5 +1,8 @@
 package it.unibo.assignment01.controller;
 
+import it.unibo.assignment01.model.Ball;
+import it.unibo.assignment01.model.Speed;
+
 /**
  * Comando inviato dalla View al Controller per applicare
  * un impulso alla pallina del giocatore umano.
@@ -20,5 +23,10 @@ public class MoveCmd implements Cmd {
 
     public double getDy() {
         return dy;
+    }
+
+    @Override
+    public void execute(Ball ball) {
+        ball.setVel(ball.getVel().sum(new Speed(dx, dy)));
     }
 }
