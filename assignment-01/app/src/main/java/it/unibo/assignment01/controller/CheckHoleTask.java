64b68c5d0606b07fabe668dba967adc05b45dc70
Luca Venturini <luca.venturini14@studio.unibo.it>
2026-05-06 15:59:54 +0200

    added checkHoleTask

---
+++ assignment-01/app/src/main/java/it/unibo/assignment01/controller/CheckHoleTask.java
@@ -0,0 +1,30 @@
+package it.unibo.assignment01.controller;
+
+import java.util.List;
+
+import it.unibo.assignment01.model.Ball;
+import it.unibo.assignment01.model.Board;
+
+public class CheckHoleTask implements Runnable {
+    private final List<Ball> ballBatch;
+    private final Board board;
+    private final Barrier barrier;
+    
+    public CheckHoleTask(List<Ball> ballBatch, Board board, Barrier barrier){
+        this.ballBatch = ballBatch;
+        this.board = board;
+        this.barrier = barrier;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Worker " + Thread.currentThread().getName() + " is working...");
+        ballBatch.stream().forEach(ball -> board.checkHole(ball));
+        try {
+            barrier.hitAndWait();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
