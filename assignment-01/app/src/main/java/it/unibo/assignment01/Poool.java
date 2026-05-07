85403af5b1552647f068abb65b143f5d1acf0a45
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-05-07 11:32:50 +0200

    Changed window dimensions

--- assignment-01/app/src/main/java/it/unibo/assignment01/Poool.java
+++ assignment-01/app/src/main/java/it/unibo/assignment01/Poool.java
@@ -6,8 +6,8 @@ import it.unibo.assignment01.controller.Barrier;
 
 public class Poool {
 
-    private static final int FRAME_WIDTH = 800;
-    private static final int FRAME_HEIGHT = 600;
+    private static final int FRAME_WIDTH = 1200;
+    private static final int FRAME_HEIGHT = 800;
     public static void main(String[] args) {
         Barrier VCBarrier = new Barrier(2);
         View view = new View(FRAME_WIDTH, FRAME_HEIGHT);
