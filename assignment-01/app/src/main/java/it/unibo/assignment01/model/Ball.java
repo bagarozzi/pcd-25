91b8c8ce90c030f44b8e04898bea4b42db465c8d
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-05-07 10:39:45 +0200

    Added radius to interface

--- assignment-01/app/src/main/java/it/unibo/assignment01/model/Ball.java
+++ assignment-01/app/src/main/java/it/unibo/assignment01/model/Ball.java
@@ -2,6 +2,9 @@ package it.unibo.assignment01.model;
 
 public interface Ball {
 
+    public static final double BALL_RADIUS = 0.02;
+    public static final double AGENT_BALL_RADIUS = 0.07;
+
     public void updateState(long dt, Board ctx);
 
     public Position getPos();
