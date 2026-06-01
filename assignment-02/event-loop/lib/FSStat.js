0e7a043db9da814509081705a43da54f7f9d0143
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-06-01 21:29:00 +0200

    Added directory scan and reporting

--- assignment-02/event-loop/lib/FSStat.js
+++ assignment-02/event-loop/lib/FSStat.js
@@ -1,3 +1,27 @@
-export function getFSReport(directory, maxFileSize, numBands) {
+import { readdir, lstat } from 'node:fs/promises';
+import { resolve } from 'node:path';
+import { Histogram } from "./Histogram.js";
 
+// Prepares the report and starts the recursive scanning of the directory
+export async function getFSReport(directory, maxFileSize, numBands) {
+    const report = new Histogram(numBands, maxFileSize);
+    await scanDirectory(directory, report);
+    return report;
 }
+
+// Recursively scans the directory and updates the report when encoutering files
+async function scanDirectory(dir, report) {
+    let path = resolve(dir);
+    const entries = await readdir(path);
+    for (const entry of entries) {
+        const entryPath = resolve(path, entry);
+        const fileStat = await lstat(entryPath);
+        if (fileStat.isFile()) {
+            report.update(fileStat.size);
+        } 
+        else if (fileStat.isDirectory()) {
+            await scanDirectory(entryPath, report);
+        }
+    }
+}
+
