24683f3f0c2fa72ad86ffbd6d4048ed7a964b266
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-06-01 21:29:10 +0200

    Added Histogram class for reporting

---
+++ assignment-02/event-loop/lib/Histogram.js
@@ -0,0 +1,36 @@
+
+// An histogram is a data structure that counts the number of file for each
+// size band. 
+export class Histogram {
+
+    constructor(numBands, maxFileSize) {
+        this.numBands = numBands;
+        this.maxFileSize = maxFileSize;
+        this.bands = new Array(numBands + 1).fill(0);
+    }
+
+    update(fileSize) {
+        let band = Math.floor(fileSize * this.numBands / this.maxFileSize);
+        if (band > this.numBands) {
+            this.bands[this.numBands]++;
+        }
+        else {
+            this.bands[band]++;
+        }
+    }
+
+    getDistribution() {
+        let dist = new Map();
+        for(let i = 0; i < this.numBands + 1; i++) {
+            let start = i * this.maxFileSize / this.numBands;
+            let end = (i == this.numBands) ? Infinity : (i + 1) * this.maxFileSize / this.numBands;
+            dist.set([start, end], this.bands[i]);
+        }
+        return dist;
+    }
+
+    getTotalFiles() {
+        return this.bands.reduce((a, b) => a + b, 0);
+    }
+
+}
