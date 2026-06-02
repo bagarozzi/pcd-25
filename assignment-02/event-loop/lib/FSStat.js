7ad5b858e14c7e6cfec390322e9e386b71df4358
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-06-02 12:39:07 +0200

    Added interactive report method

--- assignment-02/event-loop/lib/FSStat.js
+++ assignment-02/event-loop/lib/FSStat.js
@@ -1,12 +1,21 @@
 import { readdir, lstat } from 'node:fs/promises';
 import { resolve } from 'node:path';
 import { Histogram } from "./Histogram.js";
+import { DynamicFSReport } from "./DynamicFSReport.js";
 
 // Prepares the report and starts the recursive scanning of the directory
 export async function getFSReport(directory, maxFileSize, numBands) {
     const report = new Histogram(numBands, maxFileSize);
     await scanDirectory(directory, report);
     return report;
+}
+
+/**
+ * Returns a DynamicFSReport that can be used to get progressive updates of the
+ * scanned directory.
+*/
+export function getInteractiveFSReport(directory, maxFileSize, numBands) {
+    return new DynamicFSReport(directory, numBands, maxFileSize);
 }
 
 // Recursively scans the directory and updates the report when encoutering files
@@ -28,7 +37,8 @@ async function scanDirectory(dir, report) {
             report.update(stat.size);
         } 
         else if (stat.isDirectory()) {
-            const entryPath = resolve(path, entry);
+            const entryPath = resolve(path, entry)
+            report.updateDirectories();
             subdirPromises.push(scanDirectory(entryPath, report));
         }
     }
