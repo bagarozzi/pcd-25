0a1d85af72c15c6c5c8f4a93a709d28280a0dc9e
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-06-02 12:38:56 +0200

    Added private fields and directories scanned counter

--- assignment-02/event-loop/lib/Histogram.js
+++ assignment-02/event-loop/lib/Histogram.js
@@ -2,35 +2,47 @@
 // An histogram is a data structure that counts the number of file for each
 // size band. 
 export class Histogram {
+    #numBands;
+    #maxFileSize;
+    #bands;
+    #directoriesScanned = 0;
 
     constructor(numBands, maxFileSize) {
-        this.numBands = numBands;
-        this.maxFileSize = maxFileSize;
-        this.bands = new Array(numBands + 1).fill(0);
+        this.#numBands = numBands;
+        this.#maxFileSize = maxFileSize;
+        this.#bands = new Array(numBands + 1).fill(0);
     }
 
     update(fileSize) {
-        let band = Math.floor(fileSize * this.numBands / this.maxFileSize);
-        if (band > this.numBands) {
-            this.bands[this.numBands]++;
+        let band = Math.floor(fileSize * this.#numBands / this.#maxFileSize);
+        if (band > this.#numBands) {
+            this.#bands[this.#numBands]++;
         }
         else {
-            this.bands[band]++;
+            this.#bands[band]++;
         }
+    }
+
+    updateDirectories() {
+        this.#directoriesScanned++;
+    }
+
+    getScannedDirectories() {
+        return this.#directoriesScanned;
     }
 
     getDistribution() {
         let dist = new Map();
-        for(let i = 0; i < this.numBands + 1; i++) {
-            let start = i * this.maxFileSize / this.numBands;
-            let end = (i == this.numBands) ? Infinity : (i + 1) * this.maxFileSize / this.numBands;
-            dist.set([start, end], this.bands[i]);
+        for(let i = 0; i < this.#numBands + 1; i++) {
+            let start = i * this.#maxFileSize / this.#numBands;
+            let end = (i == this.#numBands) ? Infinity : (i + 1) * this.#maxFileSize / this.#numBands;
+            dist.set([start, end], this.#bands[i]);
         }
         return dist;
     }
 
     getTotalFiles() {
-        return this.bands.reduce((a, b) => a + b, 0);
+        return this.#bands.reduce((a, b) => a + b, 0);
     }
 
 }
