37feb16baf8ab4159a3ea7a50df6966d104393cb
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-06-08 15:28:10 +0200

    Added base structures of Chief, Player and Refree

--- assignment-03/odd-and-even/entity/GamePlayer.go
+++ assignment-03/odd-and-even/entity/GamePlayer.go
@@ -3,15 +3,21 @@ package entity
 // A Player is a member of the tournament.
 // You can send messages and receive messages from the player.
 type Player interface {
-	// Command the player to start a match againt an Opponent.
-	sendMatch(op Opponent)
-	receiveResults()
+	//sendMatch()
+	//receiveResults()
+	//terminate()
+	run()
 }
 
-// An Opponent is a player to which a number is sent and from which a number is received.
-type Opponent interface {
-	// Send your number to the opponent.
-	sendNumber(number int)
-	// Receive a number from your opponent.
-	receiveNumber()
+type PlayerImpl struct {
+}
+
+func CreatePlayer(id int) Player {
+	return nil
+}
+
+func (p *PlayerImpl) run() {
+	for {
+
+	}
 }
