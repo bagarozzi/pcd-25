2739e23bde6122f8b158e1b80e1fa8a84cbdfd90
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-06-08 17:30:01 +0200

    Added better argument parsing and optional logging to file

--- assignment-03/odd-and-even/main.go
+++ assignment-03/odd-and-even/main.go
@@ -5,12 +5,14 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"time"
 )
 
 var (
-	roundNum int
+	roundNum int64
 	helpFlag bool
+	logFlag  bool
 )
 
 const title string = "odds and even: goroutine odd-and-even tournament"
@@ -20,17 +22,21 @@ func main() {
 		return
 	}
 	initLogging()
-	log.Printf("Match started with %d players", roundNum)
+	log.Printf(" - [MAIN]: Tournament started with %d rounds\nPlaying...", roundNum)
 }
 
 func initLogging() {
-	timestamp := time.Now().Format("020106_1504")
-	filename := fmt.Sprintf("match_%s", timestamp)
-	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-	if err != nil {
-		log.Fatal("Failed to open log file: ", err)
+	if logFlag {
+		timestamp := time.Now().Format("020106_150405")
+		filename := fmt.Sprintf("match_%s", timestamp)
+		file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+		if err != nil {
+			log.Fatal("Failed to open log file: ", err)
+		}
+		log.SetOutput(file)
+	} else {
+		log.SetOutput(os.Stdout)
 	}
-	log.SetOutput(file)
 	log.SetFlags(log.Ldate | log.Ltime)
 }
 
@@ -38,6 +44,7 @@ func parseArguments() bool {
 	fmt.Println(title)
 	flag.BoolVar(&helpFlag, "help", false, "display the help message")
 	flag.BoolVar(&helpFlag, "h", false, "display the help message")
+	flag.BoolVar(&logFlag, "l", false, "log to separate file")
 	flag.Parse()
 	if helpFlag {
 		printUsage()
@@ -46,13 +53,22 @@ func parseArguments() bool {
 		fmt.Println(fmt.Errorf("Error: number of rounds not specified"))
 		printUsage()
 		return false
+	} else if l := len(flag.Args()); l > 1 {
+		fmt.Println(fmt.Errorf("Error: can only specify one argument, found %d", l))
+	}
+	var err error
+	if roundNum, err = strconv.ParseInt(flag.Args()[0], 10, 64); err != nil {
+		fmt.Println(fmt.Errorf("Error: number passed must be integer, found %s", flag.Args()[0]))
+		return false
 	}
 	return true
 }
 
 func printUsage() {
-	fmt.Println("\nUsage:\todds-and-even <n> [--help]")
+	fmt.Println("\nUsage:\todds-and-even [options] <n>")
 	fmt.Println("  <> n : number of rounds")
-	fmt.Println("  <> --help : display the help message")
+	fmt.Println("Options:")
+	fmt.Println("\t<> --help :\tdisplay the help message")
+	fmt.Println("\t<> --l    :\tlog to separate file")
 	fmt.Println()
 }
