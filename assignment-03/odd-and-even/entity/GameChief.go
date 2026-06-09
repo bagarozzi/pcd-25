abf3faf6ac65e8a4b51c64677f3981f66c1aac57
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-06-09 14:59:38 +0200

    Implemented channel on game termination

--- assignment-03/odd-and-even/entity/GameChief.go
+++ assignment-03/odd-and-even/entity/GameChief.go
@@ -1,22 +1,25 @@
 package entity
 
 import (
+	"context"
+	"log"
 	"math"
 	"odds-and-even/message"
+	"time"
 )
 
 type Chief interface {
-	run()
+	Run() chan int64
 	send(message.Message)
 }
 
 type GameChief struct {
-	rounds  int
+	rounds  int64
 	players []Player
 	ch      chan message.Message
 }
 
-func CreateGameChief(rounds int) Chief {
+func CreateGameChief(rounds int64, ctx context.Context) Chief {
 	var g GameChief
 	g.rounds = rounds
 	var players []Player
@@ -28,17 +31,24 @@ func CreateGameChief(rounds int) Chief {
 	return &g
 }
 
-func (g *GameChief) run() {
-	for {
-		if g.rounds == 0 {
-			//termina e segnala vincitore
+func (g *GameChief) Run() chan int64 {
+	log.Printf("[CHIEF]: starting tournament with %d rounds and %d players", g.rounds, len(g.players))
+	ch := make(chan int64)
+	go func() {
+		for {
+			if g.rounds == 0 {
+				ch <- 2
+				return
+			}
+			// matchmaking
+			// crea refree
+			// aspetta refree
+			time.Sleep(2000 * time.Millisecond)
+			// termina chi ha perso
+			g.rounds--
 		}
-		// matchmaking
-		// crea refree
-		// aspetta refree
-		// termina chi ha perso
-		g.rounds--
-	}
+	}()
+	return ch
 }
 
 func (g *GameChief) send(m message.Message) {
