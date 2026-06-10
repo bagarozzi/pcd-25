cc470d3a20451bf2542957445b8c7607423ba136
Luca Venturini <luca.venturini14@studio.unibo.it>
2026-06-10 15:50:13 +0200

    implemented the game refree logic

--- assignment-03/odd-and-even/entity/GameRefree.go
+++ assignment-03/odd-and-even/entity/GameRefree.go
@@ -12,44 +12,60 @@ type Refree interface {
 type GameRefree struct {
 	chiefChannel chan message.Message
 	players      []Player
-	results      []int
 	evenId       int
 	oddId        int
+	num1         int
+	num2         int
+	id           int
 }
 
-func CreateGameRefree() Refree {
-	return &GameRefree{}
+func CreateGameRefree(id int, players []Player, chiefCh chan message.Message) Refree {
+	return &GameRefree{
+		chiefChannel: chiefCh,
+		players:      players,
+		id:           id,
+	}
 }
 
 // Chooses a random player that chooses odd or even, then asks both players for a number
 // and claims a winners. Trasmists the winner to the Chief.
 func (g *GameRefree) run() {
-	choise := rand.IntN(2)
-	g.players[choise].getChannel() <- message.Message{MType: message.OddOrEvenType, Payload: 2}
-	m := <-g.players[choise].getChannel()
-	if m.Payload == 0 {
-		g.evenId = g.players[choise].getId()
-		g.oddId = g.players[1+choise%2].getId()
-	}
-	g.evenId = g.players[choise].getId()
-	g.players[1-choise].getChannel() <- message.Message{MType: message.OddOrEvenType, Payload: 2}
+	choise := rand.IntN(1)
+	g.players[choise].getChannel() <- message.Message{MType: message.OddOrEvenType, Payload: struct{}{}}
+	other := 1 + choise%2
+	g.players[1+choise%2].getChannel() <- message.Message{MType: message.NumberRequestType, Payload: struct{}{}}
 
-	for i, player := range g.players {
-		channel := player.getChannel()
-		mes := <-channel
-		g.results[i] = mes.Payload
+	gotNum1 := false
+	gotNum2 := false
+	for gotNum1 == false || gotNum2 == false {
+		select {
+		case msg := <-g.players[choise].getChannel():
+			if msg.Payload == 0 {
+				g.evenId = g.players[choise].getId()
+				g.oddId = g.players[1+choise%2].getId()
+			}
+			g.num1 = msg.Payload.(message.OddOrEvenReply).Number
+			gotNum1 = true
+		case msg := <-g.players[other].getChannel():
+			g.num2 = msg.Payload.(message.NumberReply).Number
+			gotNum2 = true
+		}
 	}
 
-	sum := g.results[0] + g.results[1]
+	sum := g.num1 + g.num2
 
 	if sum%2 == 0 {
 		for _, player := range g.players {
 			if player.getId() == g.evenId {
-				g.chiefChannel <- message.Message{MType: "winner", Payload: player.getId()}
+				g.chiefChannel <- message.Message{MType: message.EndMatchType, Payload: message.EndMatchReply{WinnerId: g.evenId, LoserId: g.oddId, RefreeId: g.id}}
 			}
 		}
 	} else {
-		g.chiefChannel <- message.Message{MType: "winner", Payload: g.players[1].getId()}
+		for _, player := range g.players {
+			if player.getId() == g.oddId {
+				g.chiefChannel <- message.Message{MType: message.EndMatchType, Payload: message.EndMatchReply{WinnerId: g.oddId, LoserId: g.evenId, RefreeId: g.id}}
+			}
+		}
 	}
 
 }
