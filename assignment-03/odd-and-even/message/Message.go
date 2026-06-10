145edbbeac9e9d26b8fed23fc2e3788d31163afb
Luca Venturini <luca.venturini14@studio.unibo.it>
2026-06-10 15:49:45 +0200

    added type for payload

--- assignment-03/odd-and-even/message/Message.go
+++ assignment-03/odd-and-even/message/Message.go
@@ -4,13 +4,31 @@ package message
 type MessageType string
 
 const (
-	NumberType    MessageType = "number"
-	OddOrEvenType MessageType = "choice"
-	TerminateType MessageType = "terminate"
-	EndMatchType  MessageType = "end_match"
+	NumberReplyType   MessageType = "number"
+	OddOrEvenType     MessageType = "choice"
+	NumberRequestType MessageType = "number_request"
+	TerminateType     MessageType = "terminate"
+	EndMatchType      MessageType = "end_match"
 )
 
 type Message struct {
 	MType   MessageType
-	Payload int
+	Payload any
+}
+
+type NumberReply struct {
+	Number int
+	Id     int
+}
+
+type OddOrEvenReply struct {
+	Choice int
+	Number int
+	Id     int
+}
+
+type EndMatchReply struct {
+	WinnerId int
+	LoserId  int
+	RefreeId int
 }
