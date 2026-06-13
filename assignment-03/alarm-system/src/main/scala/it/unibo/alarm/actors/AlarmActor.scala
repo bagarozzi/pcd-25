6332e5c252753375fe3dbc647bcd6fb957448be8
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-06-13 11:03:08 +0200

    Implemented base behavior of AlarmActor

--- assignment-03/alarm-system/src/main/scala/it/unibo/alarm/actors/AlarmActor.scala
+++ assignment-03/alarm-system/src/main/scala/it/unibo/alarm/actors/AlarmActor.scala
@@ -1,3 +1,42 @@
 package it.unibo.alarm.actors
 
-object AlarmActor
+import org.apache.pekko.actor.typed.{ActorRef, Behavior}
+import it.unibo.alarm.actors.SensorActor
+import it.unibo.alarm.actors.SensorActor.Type
+import org.apache.pekko.actor.typed.scaladsl.Behaviors
+
+/**
+ * The actor coordinating the whole alarm: locking, unlocking and catching signals.
+ */
+object AlarmActor:
+
+  enum Command:
+    case Arm(zone: String)
+    case ArmAll
+    case Disarm(zone: String)
+    case DisarmAll
+
+  export Command.*
+
+  def apply(zones: Map[String, Set[SensorActor.Type]]): Behavior[Command] =
+    Behaviors.setup: context =>
+      val zoneActors: Map[String, ActorRef[ZoneActor.Command]] = zones.view.map(z => (z._1, context.spawn(ZoneActor(z._2, context.self), z._1))).toMap
+      active(zoneActors, Map.empty)
+
+  private def active(disarmedZones: Map[String, ActorRef[ZoneActor.Command]], armedZones: Map[String, ActorRef[ZoneActor.Command]]): Behavior[Command] =
+    Behaviors.receive: (context, message) =>
+      message match
+        case Arm(zone) if !armedZones.contains(zone) && disarmedZones.contains(zone) => arm(Set(zone), disarmedZones.removed(zone), armedZones + (zone -> disarmedZones(zone)))
+        case Disarm(zone) if armedZones.contains(zone) && !disarmedZones.contains(zone) => disarm(Set(zone), disarmedZones + (zone -> armedZones(zone)), armedZones.removed(zone))
+        case ArmAll => arm(disarmedZones.keySet, Map.empty, armedZones ++ disarmedZones)
+        case DisarmAll => arm(armedZones.keySet, armedZones ++ disarmedZones, Map.empty)
+        case _ => active(disarmedZones, armedZones)
+
+  private def arm(zones: Set[String], disarmedZones: Map[String, ActorRef[ZoneActor.Command]], armedZones: Map[String, ActorRef[ZoneActor.Command]]): Behavior[Command] =
+    // send arm command to "zones"
+    active(disarmedZones, armedZones)
+
+  private def disarm(zones: Set[String], disarmedZones: Map[String, ActorRef[ZoneActor.Command]], armedZones: Map[String, ActorRef[ZoneActor.Command]]): Behavior[Command] =
+    // send disarm command zones
+    active(disarmedZones, armedZones)
+
