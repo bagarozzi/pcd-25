3b07dd6e3569b1b091d39a2cd6fdcfa5da19b76f
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-06-13 15:12:03 +0200

    Added comments

--- assignment-03/alarm-system/src/main/scala/it/unibo/alarm/actors/ZoneActor.scala
+++ assignment-03/alarm-system/src/main/scala/it/unibo/alarm/actors/ZoneActor.scala
@@ -3,6 +3,10 @@ package it.unibo.alarm.actors
 import org.apache.pekko.actor.typed.scaladsl.Behaviors
 import org.apache.pekko.actor.typed.{ActorRef, Behavior}
 
+/**
+ * A ZoneActor manages a zone of a house, groups together sensors from a zone and reports triggers.
+ * It can be enabled and disabled.
+ */
 object ZoneActor:
 
   import it.unibo.alarm.actors.AlarmActor.Command.*
@@ -14,6 +18,12 @@ object ZoneActor:
 
   export Command.*
 
+  /**
+   * Initialize the Zone actor and the sensors passed.
+   * @param sensors the sensors' type managed by the actor
+   * @param alarm the AlarmActor that this actor has to report to
+   * @return
+   */
   def apply(sensors: Set[SensorActor.Type], alarm: ActorRef[AlarmActor.Command]): Behavior[Command] =
     Behaviors.setup: context =>
       sensors.zipWithIndex.foreach((s, i) => context.spawn(SensorActor(context.self, s), s"sensor-$i"))
