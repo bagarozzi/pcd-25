89c5e09cceca290e34a7702915dac594ea84c7ae
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-06-13 11:07:11 +0200

    Implemented SensorActor base behavior

--- assignment-03/alarm-system/src/main/scala/it/unibo/alarm/actors/SensorActor.scala
+++ assignment-03/alarm-system/src/main/scala/it/unibo/alarm/actors/SensorActor.scala
@@ -1,3 +1,27 @@
 package it.unibo.alarm.actors
 
-object SensorActor
+import org.apache.pekko.actor.typed.*
+import org.apache.pekko.actor.typed.scaladsl.*
+
+object SensorActor:
+
+  enum Type:
+    case Motion
+    case Door
+
+  enum Command:
+    case Trigger
+
+  export Command.*
+  export Type.*
+
+  def apply(father: ActorRef[Command], sensorType: Type): Behavior[Command] =
+    Behaviors.setup: context =>
+      active(father)
+
+  private def active(father: ActorRef[Command]): Behavior[Command] =
+    Behaviors.receiveMessage:
+      case Trigger =>
+        father ! (Trigger)
+        Behaviors.same
+      case null => Behaviors.same
