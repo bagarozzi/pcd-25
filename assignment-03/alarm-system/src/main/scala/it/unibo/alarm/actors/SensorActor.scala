a064ccd9e639579abbb52ba5aba8c4f5da032f96
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-06-16 17:49:47 +0200

    Added logging and calls to Receptionist

--- assignment-03/alarm-system/src/main/scala/it/unibo/alarm/actors/SensorActor.scala
+++ assignment-03/alarm-system/src/main/scala/it/unibo/alarm/actors/SensorActor.scala
@@ -2,7 +2,9 @@ package it.unibo.alarm.actors
 
 import it.unibo.alarm.actors.ZoneActor.Command.Alert
 import org.apache.pekko.actor.typed.*
+import org.apache.pekko.actor.typed.receptionist.{Receptionist, ServiceKey}
 import org.apache.pekko.actor.typed.scaladsl.*
+import scala.reflect.ClassTag
 
 object SensorActor:
 
@@ -16,13 +18,20 @@ object SensorActor:
   export Command.*
   export Type.*
 
+  def createSensorKey[T <: SensorActor.Command : ClassTag](sensorId: String): ServiceKey[T] = {
+    ServiceKey[T](sensorId)
+  }
+
   def apply(father: ActorRef[ZoneActor.Command], sensorType: Type): Behavior[Command] =
     Behaviors.setup: context =>
-      context.log.info("Spawned " + context.self.path.toStringWithoutAddress + " of type " + sensorType.toString)
+      context.system.receptionist ! Receptionist.Register(createSensorKey("sensor"), context.self)
+      context.log.info("Spawned " + context.self.path.name + " of type " + sensorType.toString + " in zone " + context.self.path.parent.name + ".")
       active(father)
 
   private def active(father: ActorRef[ZoneActor.Command]): Behavior[Command] =
-    Behaviors.receiveMessagePartial:
-      case Trigger =>
-        father ! Alert
-        Behaviors.same
+    Behaviors.receive: (context, message) =>
+      message match
+        case Trigger =>
+          context.log.info( context.self.path.name + "-" + context.self.path.parent.name + " triggered!" )
+          father ! Alert
+          Behaviors.same
