8b97061d45a97d16290f36b5e5ea98ef4aa7ede9
Luca Venturini <luca.venturini14@studio.unibo.it>
2026-06-17 19:52:15 +0200

    added class synchCell

--- assignment-01/thread-based/app/src/main/java/it/unibo/assignment01/util/SynchCell.java
+++ assignment-01/thread-based/app/src/main/java/it/unibo/assignment01/util/SynchCell.java
@@ -1,45 +1,34 @@
 package it.unibo.assignment01.util;
 
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
 public class SynchCell<T> {
 
 	private T value;
 	private boolean available;
-	private Lock mutex;   
-	private Condition isAvail;
 
-	public SynchCell(){
+
+	public SynchCell() {
 		available = false;
-		mutex = new ReentrantLock(); 
-		isAvail = mutex.newCondition();
 	}
 
-	public void set(T v){
-		try {
-			mutex.lock();
-			value = v;
-			available = true;
-			isAvail.signalAll();  
-		} finally {
-			mutex.unlock();
+	public synchronized void set(T v) {
+		while(available) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
 		}
+		value = v;
+		available = true;
+		notifyAll();
 	}
-	
-	public T get() {
-		try {
-			mutex.lock();
-			if (!available){
-				try {
-					isAvail.await();
-				} catch (InterruptedException ex){}
-			} 
-			available = false;
-			return value;
-		} finally {
-			mutex.unlock();
+
+	public synchronized T get() throws InterruptedException {
+		while(!available) {	
+			wait();
 		}
+		available = false;
+		notifyAll();
+		return value;
 	}
 }
