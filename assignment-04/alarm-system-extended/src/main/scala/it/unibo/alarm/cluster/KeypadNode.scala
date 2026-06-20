4a1e1cf7ac8ef98a5165632c0fbfd1e8e1723c04
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-06-20 11:24:41 +0200

    Added separate role for KeypadNode to avoid initialization of shards

--- assignment-04/alarm-system-extended/src/main/scala/it/unibo/alarm/cluster/KeypadNode.scala
+++ assignment-04/alarm-system-extended/src/main/scala/it/unibo/alarm/cluster/KeypadNode.scala
@@ -12,8 +12,10 @@ import it.unibo.alarm.actors.{AlarmActor, KeypadActor, SensorActor, ZoneActor}
 object KeypadNode:
 
     def apply(keypadId: String, pin: String): ActorSystem[KeypadActor.Command] =
-        val config = ConfigFactory.parseString("pekko.remote.artery.canonical.port = 0")
-            .withFallback(ConfigFactory.load("application.conf"))
+        val config = ConfigFactory.parseString("""
+                    pekko.remote.artery.canonical.port = 0
+                    pekko.cluster.roles = ["keypad-node"]
+                    """).withFallback(ConfigFactory.load("application.conf"))
         ActorSystem(init(keypadId, pin), "AlarmCluster", config)
 
     private def init(keypadId: String, pin: String): Behavior[KeypadActor.Command] =
