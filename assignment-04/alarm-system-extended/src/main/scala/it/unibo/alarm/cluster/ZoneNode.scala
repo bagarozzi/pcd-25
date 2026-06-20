c56a955c281453b326ac6051919d7485a9c6ad44
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-06-20 09:46:54 +0200

    Added node role

--- assignment-04/alarm-system-extended/src/main/scala/it/unibo/alarm/cluster/ZoneNode.scala
+++ assignment-04/alarm-system-extended/src/main/scala/it/unibo/alarm/cluster/ZoneNode.scala
@@ -6,16 +6,13 @@ import org.apache.pekko.actor.typed.scaladsl.Behaviors
 import org.apache.pekko.cluster.sharding.typed.scaladsl.{ClusterSharding, Entity}
 
 import scala.concurrent.duration.FiniteDuration
-import it.unibo.alarm.actors.SensorActor
-import it.unibo.alarm.actors.ZoneActor
+import it.unibo.alarm.actors.{AlarmActor, SensorActor, ZoneActor}
 
 object ZoneNode:
 
     def apply(sensors: Set[SensorActor.Type], entryTimeout: FiniteDuration, exitTimeout: FiniteDuration): Unit =
-        val config = ConfigFactory.parseString(
-            s"""
-          pekko.remote.artery.canonical.port = 7354
-          """).withFallback(ConfigFactory.load("application-sharding.conf"))
+        val config = ConfigFactory.parseString("pekko.remote.artery.canonical.port = 0")
+            .withFallback(ConfigFactory.load("application.conf"))
         val _ = ActorSystem(initialization(sensors, entryTimeout, exitTimeout), "AlarmCluster", config)
 
     private def initialization(sensors: Set[SensorActor.Type], entryTimeout: FiniteDuration, exitTimeout: FiniteDuration): Behavior[Nothing] =
@@ -25,6 +22,10 @@ object ZoneNode:
             val _ = sharding.init(Entity(typeKey = ZoneActor.TypeKey) { entityContext =>
                 ZoneActor(entityContext.entityId: String, sensors: Set[SensorActor.Type], entryTimeout: FiniteDuration, exitTimeout: FiniteDuration)
             })
+
+            sharding.init(Entity(typeKey = AlarmActor.TypeKey) { entityContext =>
+                AlarmActor(entityContext.entityId, Set.empty)
+            }.withRole("central-node"))
             Behaviors.empty
 
 
